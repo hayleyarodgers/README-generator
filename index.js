baa8cd387795b903882fb02a4a8029913fa77fdc
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-07-13 15:11:40 +1000

    Write inquirer .prompt()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     'What is your GitHub username?',
     'What is your email address?',
@@ -15,6 +15,57 @@ const questions = [
     'What does the user need to know about using the repo?',
     'What does the user need to know about contributing to the repo?'
 ];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project\'s name?',  
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What does the user need to know about contributing to the repo?'
+        }
+    ])
+    .then();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
