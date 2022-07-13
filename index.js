3d8d957787f2a3415aeaa05eb127934f123c6489
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-07-13 14:46:31 +1000

    Write questions for user prompts

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your GitHub username?',
+    'What is your email address?',
+    'What is your project\'s name?',
+    'Please write a short description of your project.',
+    'What kind of license should your project have?',
+    'What command should be run to install dependencies?',
+    'What command should be run to run tests?',
+    'What does the user need to know about using the repo?',
+    'What does the user need to know about contributing to the repo?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
